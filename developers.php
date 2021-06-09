5b046c33f3f0126f0d024c90c9068bf04c09d3dd
Precious Tom <tomprezine@gmail.com>
2021-06-09 03:44:53 +0100

    update tracker from Google

--- developers.php
+++ developers.php
@@ -43,6 +43,16 @@
     <link rel="icon" href="assets/images/favicon.png" type="image/x-icon">
     <script src="assets/js/modernizr.js"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-182580885-1">
+    </script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-182580885-1');
+    </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-L6E52DCJYT"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
